a18d527f16b4d81e9086d8bd7a7acedc12535442
kjrstenholt <54651538+kjrstenholt@users.noreply.github.com>
2019-08-30 23:43:48 -0500

    Add files via upload
    initial commit of schedule viz site

---
+++ schedule_viz/index-spring.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/spring.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <div class="wrapper">
+            <?php include 'includes/header.php';?>
+
+
+            <div class="calendar_wrapper">
+                <h1>GRPH and WEBI Spring Semester 2018</h1>
+                <?php include 'includes/calendar_table.php';?>
+
+                <div class="randy_wrapper">
+                    <div class="class-box randy-tuesday">Randy: Type in Motion <br>CIST 1106-90 <br>5:30-8:20<br>Room T.4520</div>
+                    <div class="clear"></div>
+                </div>
+
+                <div class="steph_wrapper">
+                    <div class="class-box steph-monday">Stephanie: (co-teaching with Sam) Career Planning and Professional Practices<br>GRPH 2540-01, -02 <br>9-12:40<br>Room T.4530</div>
+                    <div class="class-box steph-tuesday">Stephanie: Typography 1 <br>GRPH 1101-01<br>9-12:40<br>Room T.4550, T.4520</div>
+                    <div class="class-box steph-thursday">Stephanie: Typography 1 <br>GRPH 1101-90<br>5:30-9:20<br>Room T.4550, T.4520</div>
+                    <div class="clear"></div>
+                </div>
+
+                <div class="bill_wrapper">
+                    <div class="class-box bill-wednesday">Bill: Layout and Design: The New Basics<br>GRPH 1120-01<br>12:45-4:35<br>Room T.4550, T.4520</div>
+                    <div class="class-box bill-thursday">Bill: Web Layout and Design <br>WEBI 1416-01<br>10-1:40<br>Room T.4530</div>
+                    <div class="clear"></div>
+                </div>
+
+                <div class="kjrsten_wrapper">
+                    <div class="class-box kjrsten-monday-3">Kjrsten: Intro to Graphics Software<br>GRPH 1130-90<br>5:30-9:20<br>Room T.4510</div>
+                    <div class="class-box kjrsten-tuesday">Kjrsten: Web Tools<br>WEBI 1711-01<br>9-12:40<br>Room T.4510</div>
+                    <div class="class-box kjrsten-tuesday-2">Kjrsten: Intro to Graphics Software<br>GRPH 1130-01<br>12:35-4:35<br>Room T.4510</div>
+                    <div class="class-box kjrsten-wednesday">Kjrsten: Adobe InDesign <br>GRPH 1585-01<br>9-12:40<br>Room T.4510</div>
+                    <div class="clear"></div>
+                </div>
+
+
+                <div class="will_wrapper">
+                    <div class="class-box will-monday-1">Will: (co-teaching with Sam) Portfolio Prep and Exhibit<br>GRPH 2550-01, 02<br>12:45-4:35<br>Room T.4530, T.4540</div>
+                    <div class="class-box will-monday-2">Will: Graphic Design Problems 2<br>GRPH 2530<br>5:30-9:20<br>Room T.4520, T.4550</div>
+                    <div class="class-box will-tuesday-1">Will: Print Production 1<br>GRPH 1560<br>12:45-4:35<br>Room T.4550, T.4520</div>
+                    <div class="class-box will-tuesday-2">Will: Typography 2 <br>GRPH 2101<br>5:30-9:20<br>Room T.4550, T.4510</div>
+                    <div class="class-box will-wednesday-1">Will: Adobe Photoshop <br>GRPH 2110<br>12:45-4:35<br>Room T.4530</div>
+                    <div class="class-box will-thursday">Will: Graphic Design Problems 2<br>GRPH 2530<br>10-1:40<br>Room T.4520, T.4550</div>
+                </div>
+
+                <div class="carl_wrapper">
+                    <div class="class-box carl-monday-1">Carl: CMS (Wordpress) <br>WEBI 2000<br>12:45-4:35<br>Room T.4510</div>
+                    <div class="class-box carl-monday-2">Carl: Client-Side Scripting (JS) <br>WEBI 1911<br>5:30-9:20<br>Room T.4530</div>
+                    <div class="class-box carl-tuesday">Carl: Server Side Scripting (PHP)<br>WEBI 2012<br>5:30-9:20<br>Room T.4530</div>
+                    <div class="class-box carl-wednesday-1">Carl: HTML/CSS 1 <br>WEBI 1611<br>12:45-4:35<br>Room T.4510</div>
+                    <div class="class-box carl-wednesday-2">Carl: HTML/CSS 1 <br>WEBI 1611<br>5:30-9:20<br>Room T.4510</div>
+                    <div class="class-box carl-thursday">Carl: Web Design Problems 2 <br>WEBI 2530<br>5:30-9:20<br>Room T.4530</div>
+                </div>
+
+                <div class="sam_wrapper">
+                    <div class="class-box sam-monday-1">Sam: (co-teaching with Steph) Career Planning and Professional Practices <br>GRPH 2540-01, -02<br>9-12:40<br>Room T.4540</div>
+                    <div class="class-box sam-monday-2">Sam: (co-teaching with Will) Portfolio Prep and Exhibit<br>GRPH 2550-01, 02<br>12:45-4:35<br>Room T.4540</div>
+                    <div class="class-box sam-tuesday-1">Sam: Color Theory <br>GRPH 1140<br>9-12:40<br>Room T.4530</div>
+                    <div class="class-box sam-tuesday-2">Sam: Color Theory <br>GRPH 1140<br>12:45-4:35<br>Room T.4530</div>
+                    <div class="class-box sam-wednesday-1">Sam: History of Graphic Design <br>GRPH 1540<br>12:45-3:35<br>Room T.4540</div>
+                    <div class="class-box sam-wednesday-2">Sam: Adobe Illustrator <br>GRPH 1570<br>5:30-9:20<br>Room T.4530</div>
+                </div>
+
+                <form id="faculty">
+                    <label class="randy-label"><input type="checkbox" id="randy"> Randy Johnson</label>
+                    <label class="steph-label"><input type="checkbox" id="steph"> Stephanie Glaros</label>
+                    <label class="bill-label"><input type="checkbox" id="bill"> Bill Hendricks</label>
+                    <label class="kjrsten-label"><input type="checkbox" id="kjrsten"> Kjrsten Holt</label>
+                    <label class="will-label"><input type="checkbox" id="will"> Will Lotzow</label>
+                    <label class="carl-label"><input type="checkbox" id="carl"> Carl Namyst</label>
+                    <label class="sam-label"><input type="checkbox" id="sam"> Samantha Sather</label>
+                </form>
+            </div>
+
+
+        </div>
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script>
+            $('#randy').click(function() {
+                $(".randy_wrapper").toggle(this.checked);
+            });
+        </script>
+
+    </body>
+</html>
+
+
+
+
+
+
